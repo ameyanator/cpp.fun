92df9d8903ce48c255f170ccfaa70cdecc4854d5
Ameya Sinha <ameyaosd@gmail.com>
2021-11-17 16:52:16 +0000

    test file

--- Concurrent Data Structures/BlockingQueueTest.cpp
+++ Concurrent Data Structures/BlockingQueueTest.cpp
@@ -3,14 +3,40 @@
 #include <thread>
 #include <future>
 
+using namespace std;
+
 BlockingQueue<int> bq;
+std::mutex mu;
 
 void add_to_queue(int x) {
     bq.push(x);
 }
 
-int pop_from_queue() {
+int pop_queue() {
     return bq.pop();
+}
+
+void push_into_queue() {
+    // cout<<"push_into_queue"<<endl;
+    int count = 0;
+    while(true) {
+        std::unique_lock<std::mutex> locker(mu);
+        cout<<"Pushing into queue "<<++count<<endl;
+        add_to_queue(count);
+        locker.unlock();
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+    }
+}
+
+void pop_from_queue() {
+    // cout<<"pop_from_queue"<<endl;
+    while(true) {
+        std::unique_lock<std::mutex> locker(mu);
+        std::future<int> fu = std::async(pop_queue);
+        std::cout<<"Got Value from queue "<<fu.get()<<std::endl;
+        locker.unlock();
+        std::this_thread::sleep_for (std::chrono::seconds(2));
+    }
 }
 
 void func() {
@@ -18,8 +44,10 @@ void func() {
 }
 
 int main() {
-    std::thread t1(add_to_queue, 1);
+    
     // cout<<pop_from_queue()<<endl;
-    std::future<int> fu = std::async(pop_from_queue);
-    std::cout<<fu.get()<<std::endl;
+    std::thread t1(push_into_queue);
+    std::thread t2(pop_from_queue);
+    t1.join();
+    t2.join();
 }
